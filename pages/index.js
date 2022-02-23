2514538cec95aa410c750a7185fc79cc89107684
masonkimbar <100248513+masonkimbar@users.noreply.github.com>
2022-02-22 20:58:28 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTQ_WGRtbJspyItEUa1iskOETAD8kh-Beq1gIuepwOXU12NbyDSY286fvcjzAc6Fcd8T0T8r9Dz2DCk/pubhtml?widget=true&amp;headers=false"></iframe>
       <Footer />
     </div>
   )
