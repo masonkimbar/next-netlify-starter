98160f8600d0c9c92fd478a8ca94d62ee1ff67e6
masonkimbar <100248513+masonkimbar@users.noreply.github.com>
2022-02-22 21:19:49 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,41 +3,16 @@
 
 <head>
 	<title>full screen iframe</title>
-	<style type="text/css">
-		html {
-			overflow: auto;
-		}
-		
-		html,
-		body,
-		div,
-		iframe {
-			margin: 0px;
-			padding: 0px;
-			height: 100%;
-			border: none;
-		}
-		
-		iframe {
-			display: block;
-			width: 100%;
-			border: none;
-			overflow-y: auto;
-			overflow-x: hidden;
-		}
-	</style>
 </head>
+<frameset rows="100%,*">
+	<frame src=
+"https://docs.google.com/spreadsheets/d/e/2PACX-1vTQ_WGRtbJspyItEUa1iskOETAD8kh-Beq1gIuepwOXU12NbyDSY286fvcjzAc6Fcd8T0T8r9Dz2DCk/pubhtml">
+		<noframes>
 
-<body>
-	<iframe src="https://docs.google.com/spreadsheets/d/12o1NoJOjsaCVyhl0XXmtQrq29oTNosyft0CGU80WBw4/edit#gid=0"
-			frameborder="0"
-			marginheight="0"
-			marginwidth="0"
-			width="100%"
-			height="100%"
-			scrolling="auto">
-</iframe>
+			<body>
 
-</body>
+			</body>
+		</noframes>
+</frameset>
 
 </html>
